7d1af9b057a61839af5bd4d9407407be3a0482b9
Brittany Jones <brittany.jones@brittan-ltmhtjr.internal.salesforce.com>
2025-05-12 16:47:33 -0700

    tox testing

--- hello_python/__init__.py
+++ hello_python/__init__.py
@@ -1,2 +1,8 @@
+"""
+Hello Python package.
+"""
+
+__version__ = "0.1.0"
+
 def say_hello():
     print("Hello, world!, testing! again!")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,4 @@
 from hello_python.main import main
 
 def test_main_runs():
-    # Just check that main() runs without error
     main()
-
-def main():
-    print("Hello, World!") 
